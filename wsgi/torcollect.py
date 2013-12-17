a36ededa8d04d4dca2f128b56ad6e113d3f645c5
grindhold <grindhold@skarphed.org>
2013-12-17 01:43:33 +0100

    added filestructure for torcollect webstuff

--- wsgi/torcollect.py
+++ wsgi/torcollect.py
@@ -1,13 +1,1 @@
-def application(environment, start_response):
-    response_body = ["<html><head><title>test</title></head>\
-                      <body><h1>ohai<h1></body></html>"]
-    response_headers = []
-
-    content_length = 0
-    for s in response_body:
-        content_length += len(s)
-    response_headers.append(('Content-Length', str(content_length)))
-    response_headers.append(('Content-Type', 'text/html'))
-    status = '200 OK'
-    start_response(status, response_headers)
-    return response_body
+from torcollect.web import application
