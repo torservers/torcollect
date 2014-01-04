78df054c9ff2dddf596441f8799feb0fae699c2e
grindhold <grindhold@gmx.net>
2014-01-04 16:36:37 +0100

    updated license to AGPL

--- torcollect/__init__.py
+++ torcollect/__init__.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
-#######################################
-# Torcollect
-# Released under BSD License
-# By Daniel 'grindhold' Brendle
-#######################################
+###########################################################
+# © 2011 Daniel 'grindhold' Brendle with torservers.net
+#
+# This file is part of torcollect
+#
+# torcollect is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later
+# version.
+#
+# torcollect is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE. See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with torcollect.
+# If not, see http://www.gnu.org/licenses/.
+###########################################################
 
-#######################################
-#
-# This script should be called on a
-# regular basis (24h) to collect
-# data from the servers running bridges
-#
-#######################################
 
 import sys
 import os
--- torcollect/bridge.py
+++ torcollect/bridge.py
@@ -1,6 +1,25 @@
-import rrdtool
-import torcollect.paths
+###########################################################
+# © 2011 Daniel 'grindhold' Brendle with torservers.net
+#
+# This file is part of torcollect
+#
+# torcollect is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later
+# version.
+#
+# torcollect is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE. See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with torcollect.
+# If not, see http://www.gnu.org/licenses/.
+############################################################
 
+import torcollect.paths
 
 class Bridge(object):
     def __init__(self):
@@ -9,4 +28,4 @@ class Bridge(object):
 
     def setup_db(self):
         """ Create a new database for this bridge"""
-        rrdtool.create('/tmp/test.rrd', 'DS:foo:GAUGE:20:0:U')
+	pass
