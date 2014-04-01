86742dcbc104cfdfd2131e73f9b18f0813b1f87e
grindhold <grindhold@gmx.net>
2014-04-02 00:56:49 +0200

    continued working on packaging

--- resources/pkgbuild.sh
+++ resources/pkgbuild.sh
@@ -13,6 +13,7 @@ cp -r torcollect_src torcollect/usr/lib/python2.7/dist-packages/torcollect
 cp resources/debian_package/* torcollect/DEBIAN/
 cp -r web/* torcollect/var/www_torcollect/
 cp resources/apache2_config torcollect/etc/apache2/sites-available/
+cp resources/database.sql torcollect/usr/share/torcollect/
 
 SIZE=`du -c -s torcollect/usr torcollect/var torcollect/etc | tail -n1 |  cut -f1`
 sed s/\$SIZE/$SIZE/g torcollect/DEBIAN/control > torcollect/DEBIAN/control2
